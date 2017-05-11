2d5c0e816b25198b484bb1be7add7797199e4556
Szabados Alpár <szabadosalpar@gmail.com>
2017-05-11 11:39:40 +0300

    initial commit

---
+++ app/src/main/java/buyticket/szabados/alpar/busticket/BusTicket.java
@@ -0,0 +1,13 @@
+package buyticket.szabados.alpar.busticket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_ticket);
+    }
+}
