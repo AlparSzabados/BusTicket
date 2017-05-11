c4a473803cafb7f265cbc2d25882f59530d6f98c
Szabados Alpár <szabadosalpar@gmail.com>
2017-05-11 17:46:46 +0300

    Added `AlertDialog` before sending message

--- app/src/main/java/buyticket/szabados/alpar/busticket/BusTicket.java
+++ app/src/main/java/buyticket/szabados/alpar/busticket/BusTicket.java
@@ -1,6 +1,8 @@
 package buyticket.szabados.alpar.busticket;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -36,9 +38,49 @@ public class BusTicket extends AppCompatActivity {
     }
 
     public void onClick(final View view) {
-        sendMessage(view);
-        clearMessage();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getAlertMessage(view));
+        builder.setCancelable(true);
 
+        builder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        sendMessage(view);
+                        clearMessage();
+                    }
+                });
+
+        builder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        builder.create().show();
+    }
+
+    private String getAlertMessage(View view) {
+        String sendMessagePromptText = getString(R.string.sendMessagePrompt);
+
+        switch (view.getId()) {
+            case R.id.normalTicket:
+                return sendMessagePromptText + getString(R.string.costNormalTicket);
+            case R.id.T:
+                return sendMessagePromptText + getString(R.string.costT);
+            case R.id.A:
+                return sendMessagePromptText + getString(R.string.costA);
+            case R.id.M40:
+                return sendMessagePromptText + getString(R.string.costM40);
+            case R.id.M60:
+                return sendMessagePromptText + getString(R.string.costM60);
+            case R.id.M80:
+                return sendMessagePromptText + getString(R.string.costM80);
+            case R.id.MA:
+                return sendMessagePromptText + getString(R.string.costMA);
+        }
+
+        return getString(R.string.noTicket);
     }
 
     private void sendMessage(View view) {
